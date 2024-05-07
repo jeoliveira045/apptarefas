711e8231a0f07e09b2caff24185b30f0625a3fbb
jeoliveira045 <jean.mateus.1997@gmail.com>
2024-05-06 21:37:30 -0300

    Adicionando configurações atualizadas e arquivos de CSS

--- app/src/main/java/com/labs/apptarefas/ui/theme/Color.kt
+++ app/src/main/java/com/labs/apptarefas/ui/theme/Color.kt
@@ -9,7 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val Purple700 = Color(0xFF3700B3)
+val Purple700 = Color(0xFF3600AF)
 val WHITE = Color(0xFFFFFFFF)
 val BLACK = Color(0x00000000)
 
